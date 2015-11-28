f2cb04fe2485dd7684acd03963fa0575ace17dcc
Matt Honeycutt <mbhoneycutt@gmail.com>
2015-11-27 22:09:19 -0600

    Initial commit.

---
+++ src/PSBLogPostSample.Web/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿angular.module('psBlogApp', [])
+	.run(function () {
+		console.log('Hello, world, from psBlogApp!');
+	})
+	.directive('psCurrentTime', function () {
+		return {
+			template: '<h2 class="text-center">{{vm.currentTime}}</div>',
+			controllerAs: 'vm',
+			controller: function () {
+				var vm = this;
+				vm.currentTime = new Date().toLocaleTimeString();
+			}
+		}
+	});
